c2c78e0c46c8e84a4a3fea45a26acf4551564ed3
Onkar Bhojane <122461938+onkarbhojane@users.noreply.github.com>
2025-07-06 20:41:18 +0530

    Update app.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -4,23 +4,8 @@ import userRoutes from "./routes/user.routes";
 import mailRoutes from "./routes/mail.routes";
 import transactionRoutes from "./routes/transaction.routes";
 const app = express();
-const allowedOrigins = [
-  "http://localhost:5173/",
-  "https://penta-wine.vercel.app", 
-  "https://penta-onkarbhojanes-projects.vercel.app/"
-];
 
-app.use(cors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
-      }
-    },
-    credentials: true,
-  })
-);
+app.use(cors());
 app.use(express.json());
 
 app.use("/api/mail", mailRoutes);
